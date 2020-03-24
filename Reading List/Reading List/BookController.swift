a4779d94561b3020d679a4d5395d863e91d31a40
Cody Morley <codymorleyinc@gmail.com>
2020-03-24 15:30:54 -0700

    finished initial work on book and book controller

--- Reading List/Reading List/BookController.swift
+++ Reading List/Reading List/BookController.swift
@@ -14,8 +14,27 @@ class BookController {
     
     var books: [Book] = []
     
+    var readBooks: [Book] {
+        var finishedBooks: [Book] = []
+        for book in books {
+            if book.hasBeenRead == true {
+                finishedBooks.append(book)
+            }
+        }
+        return finishedBooks
+    }
+    
+    var unreadBooks: [Book] {
+        var unfinishedBooks: [Book] = []
+        for book in books {
+            if book.hasBeenRead == false {
+                unfinishedBooks.append(book)
+            }
+        }
+        return unfinishedBooks
+    }
+    
     var readingListURL: URL? {
-        
         let fileManager = FileManager.default
         let documentsDir = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first
         let booksURL = documentsDir?.appendingPathComponent("ReadingList.plist")
@@ -29,14 +48,12 @@ class BookController {
     }
     
     func deleteBook(book: Book) {
-        
         guard let index = books.firstIndex(of: book) else { return }
         books.remove(at: index)
     }
     
     func saveToPersistentStore() {
-       
-        do {
+       do {
             let encoder = PropertyListEncoder()
             let booksPList = try encoder.encode(books)
             guard let readingListURL = readingListURL else { return }
@@ -47,7 +64,6 @@ class BookController {
     }
     
     func loadToPersistentStore() {
-        
         guard let readingListURL = readingListURL else { return }
         
         do {
@@ -60,4 +76,17 @@ class BookController {
         }
     }
     
+    func updateHasBeenRead(for book: Book) {
+        guard let bookIndex = books.firstIndex(of: book) else { return }
+        
+        if books[bookIndex].hasBeenRead == true {
+            books[bookIndex].hasBeenRead = false
+        } else if books[bookIndex].hasBeenRead == false {
+                books[bookIndex].hasBeenRead = true
+        }
+    }
+    // TO DO: - Write this function's code block
+    func updateBookStringValues(for book: Book) {
+        
+    }
 }
